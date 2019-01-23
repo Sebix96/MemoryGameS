6bf9dc883ce8d129ce4122a9aa9710596652a925
Sebix96 <44363688+Sebix96@users.noreply.github.com>
2019-01-23 23:20:30 +0100

    fix

--- app/src/main/java/pl/lodz/uni/math/sebastian/memorygames/GameActivity.java
+++ app/src/main/java/pl/lodz/uni/math/sebastian/memorygames/GameActivity.java
@@ -1,13 +1,44 @@
 package pl.lodz.uni.math.sebastian.memorygames;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class GameActivity extends AppCompatActivity {
+
+    ArrayList<String> patchList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        Bundle bundle = getIntent().getExtras();
+        assert bundle != null;
+        patchList = bundle.getStringArrayList("message");
     }
+
+    private void loadImageFromStorage(String path) {
+        try {
+            File f = new File(path, "profile.jpg");
+            Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
+            ImageView img = (ImageView) findViewById(R.id.imageButton2);
+            img.setImageBitmap(b);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void onClick(View view) {
+        loadImageFromStorage((String) patchList.get(0));
+    }
+
 }
--- app/src/main/java/pl/lodz/uni/math/sebastian/memorygames/MainActivity.java
+++ app/src/main/java/pl/lodz/uni/math/sebastian/memorygames/MainActivity.java
@@ -1,45 +1,104 @@
 package pl.lodz.uni.math.sebastian.memorygames;
 
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+    ArrayList<String> patchList =new ArrayList<String>();
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClick(View view) {
+        dispatchTakePictureIntent();
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
+    }
 
-        public void onClick(View view){
-            dispatchTakePictureIntent();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            String patch =saveToInternalStorage(imageBitmap);
+            patchList.add(patch);
         }
-        static final int REQUEST_IMAGE_CAPTURE = 1;
+    }
 
-        private void dispatchTakePictureIntent(){
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    private String saveToInternalStorage(Bitmap bitmapImage) {
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        // Create imageDir
+        File mypath = new File(directory, "profile.jpg");
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
+        return directory.getAbsolutePath();
+    }
 
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-                Bundle extras = data.getExtras();
-                Bitmap imageBitmap = (Bitmap) extras.get("data");
+    private void loadImageFromStorage(String path) {
 
-                ImageView mImageView = findViewById(R.id.imageView);
-                mImageView.setImageBitmap(imageBitmap);
-            }
+        try {
+            File f = new File(path, "profile.jpg");
+            Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
+            ImageView img = (ImageView) findViewById(R.id.imageView);
+            img.setImageBitmap(b);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-        
+
+    }
+
+    public void onClick2(View view){
+        loadImageFromStorage((String) patchList.get(0));
+    }
+
+    public void runGameActivity(View view) {
+        Intent intent = new Intent(MainActivity.this, GameActivity.class);
+        intent.putExtra("message", patchList);
+        startActivity(intent);
+    }
+
 
 }
