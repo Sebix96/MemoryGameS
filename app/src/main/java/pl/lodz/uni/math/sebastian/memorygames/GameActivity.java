ed256abc00e35249c339dcc611fd8be7e23ec67a
Sebix96 <44363688+Sebix96@users.noreply.github.com>
2019-01-24 21:19:08 +0100

    fix

--- app/src/main/java/pl/lodz/uni/math/sebastian/memorygames/GameActivity.java
+++ app/src/main/java/pl/lodz/uni/math/sebastian/memorygames/GameActivity.java
@@ -5,7 +5,10 @@ import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -20,16 +23,39 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        final GridView gridview = (GridView) findViewById(R.id.gridView);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+               
+
+                try {
+                File f = new File(patchList.get(0), "profile.jpg");
+                Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
+                ImageView img = (ImageView) v;
+                img.setImageBitmap(b);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                Toast.makeText(GameActivity.this, "" + id,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
         Bundle bundle = getIntent().getExtras();
         assert bundle != null;
         patchList = bundle.getStringArrayList("message");
+
     }
 
     private void loadImageFromStorage(String path) {
         try {
             File f = new File(path, "profile.jpg");
             Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
-            ImageView img = (ImageView) findViewById(R.id.imageButton2);
+            ImageView img = (ImageView) findViewById(R.id.imageView);
             img.setImageBitmap(b);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
---
+++ app/src/main/java/pl/lodz/uni/math/sebastian/memorygames/ImageAdapter.java
@@ -0,0 +1,53 @@
+package pl.lodz.uni.math.sebastian.memorygames;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(350, 350));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(100, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.sample_0, R.drawable.sample_0,
+            R.drawable.sample_0, R.drawable.sample_0,
+            R.drawable.sample_0, R.drawable.sample_0,
+            R.drawable.sample_0, R.drawable.sample_0,
+
+    };
+}
